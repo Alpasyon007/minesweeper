c186ac59a567e41beda9bb8cd1d8778f018f5137
Alpasyon007 <alpasyon07@gmail.com>
2017-10-25 23:32:31 +0300

    Minesweeper: Player Interactivity, Fourth Stage

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -24,10 +24,36 @@ const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) =>{
       //will fix placing bombs on already existing bombs
       let randomRowIndex = Math.floor(Math.random() * numberOfRows);
       let randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
-      board[randomRowIndex][randomColumnIndex] = "B";
-      numberOfBombsPlaced++;
+      if(board[randomRowIndex][randomColumnIndex] !== "B"){
+        board[randomRowIndex][randomColumnIndex] = "B";
+        numberOfBombsPlaced++;
+      }
     }
   return board;
+}
+
+const getNumberOfNeighbotBombs = (bombBoard, rowIndex, columnIndex) =>{
+  const neighbourOffsets = [[-1, 1],[-1, 0],[-1, 1],[0 ,-1],[0, 1],[1, -1],[1, 0],[1, 1]];
+  const numberOfRows = bombBoard.length;
+  const numberOfColumns = bombBoard[0].length;
+  let numberOfBombs = 0;
+  neighbourOffsets.forEach(offset =>{const neighborRowIndex = rowIndex + offset[0];
+  const neighborColumnIndex = columnIndex + offset[1];
+if(neighborRowIndex >= 0 && neighborRowIndex < numberOfRows && neighborColumnIndex >= 0 && neighborColumnIndex < numberOfColumns){
+  if(bombBoard[neighborRowIndex][neighborColumnIndex] === "B"){
+    numberOfBombs++;
+  }}});
+  return numberOfBombs
+};
+
+const flipTile = (playerBoard, bombBoard, rowIndex, columnIndex) =>{
+  if(playerBoard[rowIndex][columnIndex] !== " "){
+  return "This tile has already been flipped!"
+  }else if (bombBoard[rowIndex][columnIndex] === "B") {
+    playerBoard[rowIndex][columnIndex] = "B";
+  }else {
+    playerBoard[rowIndex][columnIndex] = getNumberOfNeighbotBombs(bombBoard, rowIndex, columnIndex);
+  }
 }
 
 const printBoard = (board) =>{
@@ -41,3 +67,7 @@ console.log("Player Board: ");
 printBoard(playerBoard);
 console.log("Bomb Board: ");
 printBoard(bombBoard);
+
+flipTile(playerBoard, bombBoard, 0, 0)
+console.log("Updated Player Board: ");
+printBoard(playerBoard);
